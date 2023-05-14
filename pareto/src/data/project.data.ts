4b4f1d847ba04642d5429d1871902895c615d1a3
corno <corno.astn.a@schraverus.com>
2023-05-14 13:34:40 +0200

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -19,7 +19,7 @@ import { external, submodule, tempSubmodule, this_ } from "lib-pareto-typescript
 
 export const $: g_project.T.Project<pd.SourceLocation | null> = {
     'author': "Corno",
-    'description': "the typesystem for the imperative Proto language",
+    'description': "the skeleton datamodel of Alan (no references)",
     'license': "TBD",
 
     'dependencies': d({
