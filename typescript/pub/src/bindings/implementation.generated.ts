b2414bcb6a13d4f9bff049059214e2ea425d1fc0
Corno Schraverus <corno@schraverus.com>
2024-09-15 11:01:07 +0200

    .

--- typescript/pub/src/bindings/implementation.generated.ts
+++
@@ -1,8 +0,0 @@
-import { API } from "./api.generated"
-import { $$ as iresolve } from "./implementations/resolve.b"
-import { $$ as iserialize } from "./implementations/serialize.b"
-
-export const $api: API = {
-    'resolve': iresolve,
-    'serialize': iserialize,
-}
